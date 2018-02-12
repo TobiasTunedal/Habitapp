28c9ec99808b93482fbd2b637259223d4cf55437
Tobias Tunedal <tunedal.tobias@hotmail.com>
2018-02-12 13:22:22 +0100

    meny i hörnet

--- App.js
+++ App.js
@@ -16,27 +16,53 @@ export default class App extends React.Component {
     let pic = {
       uri: 'https://t4.ftcdn.net/jpg/01/60/56/39/240_F_160563921_kVvQMkxGXdZrqa9EFNpA6bd7Ctaq0hSb.jpg'
     };
+
     return (
+
       <View style={styles.container}>
+      
+      <View style={[styles.hamburgerMenu]}>
       <Hamburger active={this.state.active}
       type="cross"
       onPress={()=> this.setState({active: !this.state.active})} />
-        <Image source={pic} style={{width: 193, height: 210}}/>
+      </View>
+    
+      <View style={[styles.boxcontainer]}>
+        <Image source={pic} style={styles.logo}/>
         <Text>Welcome!</Text>
         <Text>To Habitapp</Text>
-        
+      </View>
+
       </View>
       
     );
+
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'column',
+  },
+
+  boxcontainer: {
+    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
    
   },
+  hamburgerMenu: {
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  logo: {
+    width: 240,
+    height: 260,
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  
 });
