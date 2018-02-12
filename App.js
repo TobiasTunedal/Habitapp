237abafef1aff96f5fbbb32be0e011f9c5eba37b
NoaSironen <noa.sironen@gmail.com>
2018-02-12 16:40:39 +0100

    saved file

--- App.js
+++ App.js
@@ -7,13 +7,7 @@ export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-<<<<<<< HEAD
         <Welcome/>
-=======
-        <Text>Welcome!</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Vi mår i Habitapps</Text>
->>>>>>> 4a28b19f7ed6308a81d6e4d05b2aca39c696181d
       </View>
     );
   }
