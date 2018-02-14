41b1d88103fda2c353eb587a0de3ec7eb40a45e7
Tobias Tunedal <tunedal.tobias@hotmail.com>
2018-02-14 14:59:25 +0100

    master change

--- App.js
+++ App.js
@@ -1,74 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-import { AppRegistry, Image } from 'react-native';
-import Hamburger from 'react-native-hamburger';
 
 import Welcome from './Welcome';
 
 export default class App extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      active: false
-    }
-  }
-  
   render() {
-    let pic = {
-      uri: 'https://t4.ftcdn.net/jpg/01/60/56/39/240_F_160563921_kVvQMkxGXdZrqa9EFNpA6bd7Ctaq0hSb.jpg'
-    };
-
     return (
-
       <View style={styles.container}>
-<<<<<<< HEAD
-      
-      <View style={[styles.hamburgerMenu]}>
-      <Hamburger active={this.state.active}
-      type="cross"
-      onPress={()=> this.setState({active: !this.state.active})} />
-      </View>
-    
-      <View style={[styles.boxcontainer]}>
-        <Image source={pic} style={styles.logo}/>
-        <Text>Welcome!</Text>
-        <Text>To Habitapp</Text>
-      </View>
-
-=======
         <Welcome/>
->>>>>>> master
       </View>
-      
     );
-
   }
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    flexDirection: 'column',
-  },
-
-  boxcontainer: {
-    flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-   
   },
-  hamburgerMenu: {
-    alignItems: 'flex-start',
-    justifyContent: 'flex-start',
-  },
-  logo: {
-    width: 240,
-    height: 260,
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-  
 });
