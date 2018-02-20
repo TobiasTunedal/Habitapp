ac6a8eeac4a414c72bc5a272c0e537a7d82155ec
NoaSironen <noa.sironen@gmail.com>
2018-02-20 14:27:45 +0100

    Setup index.js connection to react-native-firebase.

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
-
 import Welcome from './Welcome';
+
 
 export default class App extends React.Component {
   render() {
